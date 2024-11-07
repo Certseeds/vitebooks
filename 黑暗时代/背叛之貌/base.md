cf7843f5e692ba99c07417ac9556d38fd03aef76
Certseeds <51754303+Certseeds@users.noreply.github.com>
2024-11-07 14:59:26 +0000

    feat: add HH16-背叛之貌
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ 黑暗时代/背叛之貌/base.md
@@ -0,0 +1,18 @@
+---
+prev:
+  text: '黑暗时代-背叛之貌-meta'
+  link: '/黑暗时代/背叛之貌/meta'
+next:
+  text: '黑暗时代-背叛之貌-src'
+  link: '/黑暗时代/背叛之貌/src'
+---
+
+# 黑暗时代-背叛之貌-序章
+
+作者: Gav Thorpe
+
+On The Cover: The Raven Guard push through the traitors encamped in the Isstvan System.
+
+## 校者备注
+
+1. 阿尔法军团, 实际恐怖!
---
+++ 黑暗时代/背叛之貌/module.js
@@ -0,0 +1,13 @@
+const path = "/黑暗时代/背叛之貌";
+// 生成链接的函数
+
+const replaceDirs = [];
+const repalceFiles = [`.${path}/src.md`,];
+const repalceNameFiles = [`.${path}/names.txt`,];
+
+
+module.exports = {
+    replaceDirs: replaceDirs,
+    repalceFiles: repalceFiles,
+    repalceNameFiles: repalceNameFiles
+}
