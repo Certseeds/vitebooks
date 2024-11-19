4b107294414905b342a74816591eb91f04055afe
Certseeds <51754303+Certseeds@users.noreply.github.com>
2024-11-19 15:17:52 +0000

    feat: add HH20-破碎倒影
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ 原体/破碎倒影/module.js
@@ -0,0 +1,57 @@
+const path = "/原体/破碎倒影";
+// 生成链接的函数
+// TODO, 统一generateLink && generatePattern用到的函数
+
+const order = {
+    begin: 1,
+    end: 14,
+}
+const numToStr = (num) => {
+    if (order.end >= 10) {
+        return num.toString().padStart(2, '0');
+    } else if (order.end < 10) {
+        return chapter.toString();
+    }
+}
+const generateLink = (chapter) => {
+    const chapterNumber = numToStr(chapter);
+    return {
+        text: `Chapter ${chapterNumber}`,
+        link: `${path}/src/chapter${chapterNumber}`
+    };
+}
+const prelink = `${path}/base`;
+
+const replaceDirs = [`.${path}/src`,];
+const repalceFiles = [];
+const repalceNameFiles = [`.${path}/names.txt`,];
+
+const resource = `.${path}/HH20破碎倒影.txt`;
+
+const IsChapterBegin = (line) => {
+    const singleDigitRegex = /^\d{1,2}$/;
+    return singleDigitRegex.test(line);
+}
+const head = (chapter) => {
+    const chapterNumber = numToStr(chapter);
+    return `# 原体-破碎倒影-正文-chapter${chapterNumber}`
+}
+const generatePattern = (chapter) => {
+    const chapterNumber = numToStr(chapter);
+    return ["src", `chapter${chapterNumber}.md`];
+}
+
+module.exports = {
+    order: order,
+    generateLink: generateLink,
+    prelink: prelink,
+
+    replaceDirs: replaceDirs,
+    repalceFiles: repalceFiles,
+    repalceNameFiles: repalceNameFiles,
+
+    resource: resource,
+    IsChapterBegin: IsChapterBegin,
+    head: head,
+    generatePattern: generatePattern
+}
