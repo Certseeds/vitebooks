fd65e2cb45d47a1293e6be0dd9ec75bbd19a005d
Certseeds <51754303+Certseeds@users.noreply.github.com>
2025-03-15 04:32:50 +0000

    feat: add HH31-狼爪
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ 背叛之遗/狼爪/base.md
@@ -0,0 +1,18 @@
+---
+prev:
+  text: 'meta'
+  link: '/背叛之遗/狼爪/meta'
+next:
+  text: 'src'
+  link: '/背叛之遗/狼爪/src'
+---
+
+# 背叛之遗-狼爪-序章
+
+作者: Chris Wraight
+
+## 出场人物
+
+比约恩, 断手
+
+斯莱杰克, 铸造大师
---
+++ 背叛之遗/狼爪/module.js
@@ -0,0 +1,11 @@
+const path = "/背叛之遗/狼爪";
+
+const replaceDirs = [];
+const repalceFiles = [`.${path}/base.md`, `.${path}/src.md`,];
+const repalceNameFiles = [`.${path}/names.txt`,];
+
+export {
+    replaceDirs,
+    repalceFiles,
+    repalceNameFiles,
+}
