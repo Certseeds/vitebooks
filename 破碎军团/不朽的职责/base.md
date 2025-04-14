c2ad6f2379af83ad4882ffc29a049b69661e703f
Certseeds <51754303+Certseeds@users.noreply.github.com>
2025-04-14 13:05:24 +0000

    feat: add HH43-不朽的职责
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ 破碎军团/不朽的职责/base.md
@@ -0,0 +1,14 @@
+---
+prev:
+  text: 'meta'
+  link: '/破碎军团/不朽的职责/meta'
+next:
+  text: 'src'
+  link: '/破碎军团/不朽的职责/src'
+---
+
+# 破碎军团-不朽的职责-序章
+
+作者: Nick Kyme
+
+
---
+++ 破碎军团/不朽的职责/module.js
@@ -0,0 +1,11 @@
+const path = "/破碎军团/不朽的职责";
+
+const replaceDirs = [];
+const repalceFiles = [`.${path}/base.md`, `.${path}/src.md`,];
+const repalceNameFiles = [`.${path}/names.txt`,];
+
+export {
+    replaceDirs,
+    repalceFiles,
+    repalceNameFiles,
+}
