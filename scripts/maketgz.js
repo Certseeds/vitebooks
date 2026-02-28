09632b613ab2cca39d0a3195911377efd186aa5e
Certseeds <51754303+Certseeds@users.noreply.github.com>
2026-02-28 10:35:49 +0000

    feat: 替换 archiver to tar
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- scripts/maketgz.js
+++ scripts/maketgz.js
@@ -2,11 +2,10 @@
 // SPDX-FileCopyrightText: 2024-2025 Certseeds
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import fs from 'node:fs';
 import fsp from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import archiver from 'archiver';
+import { create } from 'tar';
 import { exec } from 'child_process';
 import { domainName } from './domain.js';
 
@@ -17,18 +16,16 @@ console.log(args);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const findMetaFiles = async (dir, tgz) => {
+const findMetaFiles = async (dir, filePaths) => {
     const files = await fsp.readdir(dir);
     for (const file of files) {
         const fullPath = path.join(dir, file);
         const stat = await fsp.stat(fullPath);
         if (stat.isDirectory() && (isChinese(file) || file === 'welldone')) {
-            await findMetaFiles(fullPath, tgz);
+            await findMetaFiles(fullPath, filePaths);
         } else if (file === 'meta.toml') {
             console.log(dir, fullPath);
-            const content = await fsp.readFile(fullPath);
-            // console.log(content.toString());
-            tgz.append(Buffer.from(content, "utf8"), { name: fullPath });
+            filePaths.push(fullPath);
             break;
         }
     }
@@ -61,15 +58,18 @@ const toepub = async (dir) => {
     }
 };
 const scanAndZip = async () => {
-    const output = fs.createWriteStream('./public/meta.tgz');
-    const tgz = archiver('tar', {
-        gzip: true,
-        zlib: { level: 9 }
-    });
-    tgz.pipe(output);
-    await findMetaFiles(".", tgz);
-    tgz.finalize();
-    await toepub(".");
+    const filePaths = [];
+    await findMetaFiles('.', filePaths);
+    await create(
+        {
+            gzip: { level: 9 },
+            file: './public/meta.tgz',
+            portable: true,
+            strict: true
+        },
+        filePaths
+    );
+    await toepub('.');
 };
 
 scanAndZip().catch(err => console.error(err));
